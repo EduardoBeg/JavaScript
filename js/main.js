a52902aa70a7e75eb5c7e1e8664c3b5fcf165902
Eduardo Begnini <carlosbegnini7@gmail.com>
2020-07-20 10:34:01 -0300

    Rascunho de JavaScript

---
+++ js/main.js
@@ -0,0 +1,118 @@
+function clikado(){
+    //alert("Virus Detectado");
+  document.getElementById("agradecimento").innerHTML = "<b>Tem que ser idiota</b>";
+}
+function redirecionar(){
+    //window.open("google.com");
+    window.location.href = "https://google.com"
+}
+function trocar(elemento){
+    elemento.innerHTML ="Obrigado por passar o mupse";
+}
+function voltar(elemento){
+    elemento.innerHTML="Thau brigado";
+}
+function load(){
+    alert("acabouuuuu");
+}
+function funcaoChange(elemento){
+    console.log(elemento.value);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*function soma (n1, n2){
+    return n1 + n2;
+
+
+}
+function validaidade (idade){
+    var validar
+    if (idade >= 18){
+    validar = true;
+}else{
+    validar=false;
+}
+return validar;
+}
+var idade = prompt ("Qual sua idade?");
+console.log(validaidade(idade));
+
+
+
+alert (soma(5, 10));
+
+
+
+
+
+var nome ="Carlos";
+var n1 = 43;
+var n2 = 56;
+var frase = "Lasanha é gostosa pkrl";
+
+alert( nome + " tem " + idade + " anos")
+
+console.log(nome);
+console.log(n1-n2);
+console.log(frase.toLowerCase());
+//alert(frase.replace("lasanha", "pizza"));
+*/
+///var lista =["maça", "pera", "laranja"];
+//lista.push("uva");
+//lista.pop();
+//console.log(lista);
+//console.log(lista.toString());
+//console.log(lista.join(""));
+/*
+var fruta = {nome:"maça", cor:"rosinha"}
+console.log(fruta.cor);
+*/
+//var fruta = [{nome:"maça", cor:"rosinha"}, {nome:"manga", cor:"preta"}]
+//console.log(fruta);
+//alert(fruta[1].nome); 
+/*
+var idade = prompt("Qual a sua idade");
+if (idade >= 18){
+    alert("Maior de idade");
+}else{
+    alert("Menor de idade");
+}*/
+/*
+var count = 0;
+while (count <= 5 ){
+    console.log(count);
+    alert(count);
+    count++;
+}
+
+var count;
+for(count = 1 ;count<= 5;count++){
+    alert (count);
+};
+
+var d = new Date();
+alert(d.getDay());
+alert(d.getHours());
+*/
